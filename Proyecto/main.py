997918c88fba06cfb73e5f91dcc61c9a26a1d84f
SergioRosello <sergio-rosello@hotmail.com>
2018-04-29 21:20:53 +0200

    Done for today

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -3,8 +3,11 @@ import os
 
 
 if __name__ == '__main__':
-    # Adquiere el bus de la sesion del sistema
+    # The *system bus* is global and usually started during boot; it's used to
+    # communicate with system services like udev_, NetworkManager_, and the
+    # `Hardware Abstraction Layer daemon (hald)`_.
     bus = dbus.SystemBus()
+
     # To obtain a proxy object, call the ``get_object`` method on the ``Bus``.
     # For example, NetworkManager_ has the well-known name
     # ``org.freedesktop.NetworkManager`` and exports an object whose object
@@ -17,6 +20,8 @@ if __name__ == '__main__':
     # Well known name: org.freedesktop.login1
     # Object path: /org/freedesktop/login1
     mgr = bus.get_object('org.freedesktop.login1', '/org/freedesktop/login1')
+    # An interface is a group of related methods and signals (more on signals later), 
+    # identified by a name which is a series of dot-separated components starting with a reversed domain name
     mgr_iface = dbus.Interface(mgr, dbus_interface='org.freedesktop.login1.Manager')
     user = bus.get_object('org.freedesktop.login1', mgr_iface.GetUserByPID(os.getpid()))
     uuid = user.Get('org.freedesktop.login1.User', 'UID', dbus_interface='org.freedesktop.DBus.Properties')
